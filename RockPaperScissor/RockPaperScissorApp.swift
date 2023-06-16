872c245eca5251c58bbda9586113315132818d7d
Tuğberk Ata <atatugberk@yahoo.com>
2023-06-16 22:42:16 +0300

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Tuğberk Ata on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
